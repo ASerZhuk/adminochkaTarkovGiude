162e8b79481ad0862eee4640b8a920d3fef2e1d9
ProgTonar <programmisttonar@gmail.com>
2025-11-06 22:14:10 +0300

    fix icons

--- app/dashboard/map-icons/page.tsx
+++ app/dashboard/map-icons/page.tsx
@@ -5,3 +5,7 @@ export default async function MapIconsPage() {
   const icons = await getIcons();
   return <MapIconsClient icons={icons} />;
 }
+
+// Всегда получать свежий список иконок без кеша
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
